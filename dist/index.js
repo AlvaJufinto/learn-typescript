3e62e1a1996438bcea410c05d85a05386a1254fe
unknown <yeahbruh199@gmail.com>
2022-06-30 20:56:53 +0700

    First step

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,12 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age = +10;
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+kgToLbs(10);
+kgToLbs('10kg');
 //# sourceMappingURL=index.js.map
